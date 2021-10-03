bb760f6e8b761a923b195c875c7d709898b583f2
Ivan <ivan.markov.2013@gmail.com>
2021-10-03 13:39:10 +0300

    0.0.6 added options menu

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,8 +8,10 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     date1 = new Date();
     date2 = new Date();
-    format1 = true;
-    format2 = true;
+    format = true;
+
+    connect(ui->defaultFormat, SIGNAL(triggered()), this, SLOT(setDefaultFormat()));
+    connect(ui->americanFormat, SIGNAL(triggered()), this, SLOT(setAmericanFormat()));
 }
 
 MainWindow::~MainWindow()
@@ -19,133 +21,109 @@ MainWindow::~MainWindow()
     delete date2;
 }
 
+void MainWindow::setDefaultFormat()
+{
+    if (!format)
+    {
+        ui->americanFormat->setChecked(false);
+        format = true;
+        if (date1->getIsDate())
+            showDate1();
+        if (date2->getIsDate())
+            showDate2();
+    }
+}
+
+void MainWindow::setAmericanFormat()
+{
+    if (format)
+    {
+        ui->defaultFormat->setChecked(false);
+        this->format = false;
+        if (date1->getIsDate())
+            showDate1();
+        if (date2->getIsDate())
+            showDate2();
+    }
+}
+
 
 void MainWindow::on_dateInput1_clicked()
 {
     date1->buildDate(ui->dateEdit1->toPlainText());
-    if (format1)
-    {
-        ui->dateLabel1->setText(date1->toDefaultString());
-    }
-    else
-    {
-        ui->dateLabel1->setText(date1->toAmericanString());
-    }
+    showDate1();
 
     if (date1->getIsDate())
     {
         ui->incrementDate1->setEnabled(true);
         ui->decrementDate1->setEnabled(true);
-
-        ui->defaultFormat1->setEnabled(true);
-        ui->americanFromat1->setEnabled(true);
     }
     else
     {
         ui->incrementDate1->setEnabled(false);
         ui->decrementDate1->setEnabled(false);
-
-        ui->defaultFormat1->setEnabled(false);
-        ui->americanFromat1->setEnabled(false);
     }
+}
+
+void MainWindow::showDate1()
+{
+    if (format)
+        ui->dateLabel1->setText(date1->toDefaultString());
+    else
+        ui->dateLabel1->setText(date1->toAmericanString());
 }
 
 
 void MainWindow::on_dateInput2_clicked()
 {
     date2->buildDate(ui->dateEdit2->toPlainText());
-    if (format2)
-    {
-        ui->dateLabel2->setText(date2->toDefaultString());
-    }
-    else
-    {
-        ui->dateLabel2->setText(date2->toAmericanString());
-    }
+    showDate2();
 
     if (date2->getIsDate())
     {
         ui->incrementDate2->setEnabled(true);
         ui->decrementDate2->setEnabled(true);
-
-        ui->defaultFormat2->setEnabled(true);
-        ui->americanFormat2->setEnabled(true);
     }
     else
     {
         ui->incrementDate2->setEnabled(false);
         ui->decrementDate2->setEnabled(false);
-
-        ui->defaultFormat2->setEnabled(false);
-        ui->americanFormat2->setEnabled(false);
     }
+}
+
+void MainWindow::showDate2()
+{
+    if (format)
+        ui->dateLabel2->setText(date2->toDefaultString());
+    else
+        ui->dateLabel2->setText(date2->toAmericanString());
 }
 
 void MainWindow::on_incrementDate1_clicked()
 {
     date1->increment();
-    ui->dateLabel1->setText(date1->toDefaultString());
+    showDate1();
 }
 
 
 void MainWindow::on_decrementDate1_clicked()
 {
     date1->decrement();
-    ui->dateLabel1->setText(date1->toDefaultString());
+    showDate1();
 }
 
 
 void MainWindow::on_incrementDate2_clicked()
 {
     date2->increment();
-    ui->dateLabel2->setText(date2->toDefaultString());
+    showDate2();
 }
 
 
 void MainWindow::on_decrementDate2_clicked()
 {
     date2->decrement();
-    ui->dateLabel2->setText(date2->toDefaultString());
-}
-
-
-void MainWindow::on_defaultFormat1_clicked()
-{
-    if(!format1)
-    {
-        format1 = true;
-        ui->dateLabel1->setText(date1->toDefaultString());
-    }
-}
-
-
-void MainWindow::on_americanFromat1_clicked()
-{
-    if(format1)
-    {
-        format1 = false;
-        ui->dateLabel1->setText(date1->toAmericanString());
-    }
-}
-
-
-void MainWindow::on_defaultFormat2_clicked()
-{
-    if(!format2)
-    {
-        format2 = true;
-        ui->dateLabel2->setText(date2->toDefaultString());
-    }
-}
-
-
-void MainWindow::on_americanFormat2_clicked()
-{
-    if(format2)
-    {
-        format2 = false;
-        ui->dateLabel2->setText(date2->toAmericanString());
-    }
+    showDate2();
 }
 
 
@@ -171,4 +149,3 @@ void MainWindow::on_decreaseDate2_clicked()
 {
 
 }
-
--- mainwindow.h
+++ mainwindow.h
@@ -29,14 +29,6 @@ private slots:
 
     void on_decrementDate2_clicked();
 
-    void on_defaultFormat1_clicked();
-
-    void on_americanFromat1_clicked();
-
-    void on_defaultFormat2_clicked();
-
-    void on_americanFormat2_clicked();
-
     void on_increaseDate1_clicked();
 
     void on_decreaseDate1_clicked();
@@ -45,11 +37,17 @@ private slots:
 
     void on_decreaseDate2_clicked();
 
+    void setDefaultFormat();
+
+    void setAmericanFormat();
+
 private:
     Ui::MainWindow *ui;
     Date* date1;
     Date* date2;
-    bool format1;
-    bool format2;
+    bool format;
+
+    void showDate1();
+    void showDate2();
 };
 #endif // MAINWINDOW_H
